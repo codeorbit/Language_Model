5974b61f95c91a6c421c049e8ab9e5da5e6d58b4
akhil <akhilgupta.official@gmail.com>
2016-01-29 16:54:03 +0530

    import middleware fixed

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from bottle import route,run,response
-from main import autoMain
+from middleware import autoMain
 @route('/')
 def hello():
 	return "Welcome to the language model"
@@ -14,14 +14,6 @@ def automplete_show(name = " "):
 	response.content_type = 'application/json'   
 	response.set_header('Cache-Control', 'no-cache')
    	return {"content": autoMain(name)}
-
-@route('/autocomplete/<name>', method='DELETE' )
-def node_delete( name=" " ):
-    return "DELETE RECIPE " + name
-
-@route('/index/<word>', method='PUT')
-def node_save( name=" " ):
-    return "SAVE RECIPE " + name
 
 run(host='localhost', port=7777, debug=True)
 
