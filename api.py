3273bd4c5f7ac85fa8da3c300e168f851769d457
akhil <akhilgupta.official@gmail.com>
2016-02-07 22:33:10 +0530

    added route for getting tags

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from bottle import route,run,response
 from middleware import autoMain
+import tags
 @route('/')
 def hello():
 	return "Welcome to the language model"
@@ -7,6 +8,15 @@ def hello():
 @route('/language_model/')
 def default_list():
     return "Welcome to the language model"
+
+@route('/get_tags/<name>/<top_n>',method = 'GET')
+def get_tags(name = " ",top_n = " "):
+	
+	return {"tags":(tags.tagsMain(name,top_n))}
+
+@route('/get_tags/<name>',method = 'GET')
+def get_tags(name = " "):
+	return {"tags":(tags.tagsMain(name,5))}
 
 @route('/autocomplete/<name>', method='GET')
 def automplete_show(name = " "):
