68d1740f56a4a05c950ebf64092aa1b711bac243
akhil <akhilgupta.official@gmail.com>
2016-02-09 19:41:02 +0530

    individual word peter norvig spellcheck

--- spellcheck.py
+++ spellcheck.py
@@ -1,19 +1,16 @@
-import re
-import collections 
 
+import re, collections
 
-def words(text):
-	return re.findall('[a-z]+',text.lower())
+def words(text): return re.findall('[a-z]+', text.lower()) 
 
 def train(features):
-	# lambda:1 is done for smoothing
-	model = collections.defaultdict(lambda:1) 
-	for f in features:
-		model[f]+=1
-	return model
+    model = collections.defaultdict(lambda: 1)
+    for f in features:
+        model[f] += 1
+    return model
 
-nwords = train(words(a))
-# print nwords['hello']
+NWORDS = train(words(file('big.txt').read()))
+
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 def edits1(word):
@@ -23,6 +20,16 @@ def edits1(word):
    replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
    inserts    = [a + c + b     for a, b in splits for c in alphabet]
    return set(deletes + transposes + replaces + inserts)
-def edits2(word):
-    return set(e2 for e1 in edits1(word) for e2 in edits1(e1))
-print list(edits2("hello"))	[0:10]
+
+def known_edits2(word):
+    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+
+def known(words): return set(w for w in words if w in NWORDS)
+
+def correct(word):
+    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
+  
+    c = list(candidates)
+    return c
+    # print max(candidates, key=NWORDS.get)
+# correct('vaina')
